9d219cb3c7af5fba89840241b6f77244ae9e8ed4
Brendan Bobryk <babobryk@gmail.com>
2024-10-04 12:31:26 -0700

    Created input validation

--- combination.cpp
+++ combination.cpp
@@ -5,7 +5,11 @@ using namespace std;
 /*
  * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
  */
-uint16_t factorial(const uint16_t x) { return x; }
+uint16_t factorial(const uint16_t x)
+{
+
+    return x;
+}
 
 /*
  * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
@@ -13,10 +17,18 @@ uint16_t factorial(const uint16_t x) { return x; }
 int main()
 {
     int n;
+    int k;
 
     // get and validate user input
     cout << "Enter n: ";
     cin >> n;
+    cout << "Enter k: ";
+    cin >> k;
+    if (n <= 0 || k <= 0)
+    {
+        cout << "Invalid input. Please only enter positive integers." << endl;
+        return -1;
+    }
 
     // calculate C(n,k) = n! / (k! * (n-k)!)
     uint16_t c_n_k = factorial(n);
